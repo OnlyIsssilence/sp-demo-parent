6440fd33f1f1c49951a52291d41cc3ecf1a33546
MuYa <981411866@qq.com>
2020-07-02 10:47:55 +0800

    ADD: elk测试

---
+++ sp-demo-elk/src/main/java/com/muya/elk/SpDemoElkApplication.java
@@ -0,0 +1,13 @@
+package com.muya.elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpDemoElkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpDemoElkApplication.class, args);
+    }
+
+}
---
+++ sp-demo-elk/src/test/java/com/muya/elk/SpDemoElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muya.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpDemoElkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
