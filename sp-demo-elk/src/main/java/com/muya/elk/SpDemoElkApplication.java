71d7f4a4653a5f5109100483bb106925345b1189
MuYa <981411866@qq.com>
2020-08-11 13:56:24 +0800

    ADD: Excel demo测试模块

--- sp-demo-elk/src/main/java/com/muya/elk/SpDemoElkApplication.java
+++ sp-demo-elk/src/main/java/com/muya/elk/SpDemoElkApplication.java
@@ -3,6 +3,14 @@ package com.muya.elk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Copyright 2020 OnlySilence, Inc. All rights reserved.
+ *
+ * @Author: MuYa
+ * @Date: 2020-06-21
+ * @Time: 23:27
+ * @Description:
+ */
 @SpringBootApplication
 public class SpDemoElkApplication {
 
